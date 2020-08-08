3a0b6c565ac163bf554213ee3407df0a8d878cda
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-08 17:18:52 +0900

    [Finish]AdvancedWork6 Ajax Favorite Comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,15 +6,15 @@ class BookCommentsController < ApplicationController
     @book_comment.book_id = @book.id
     if @book_comment.save
       flash[:success] = "Comment was successfully created."
+    else
+      @book_comments = BookComment.where(id: @book)
     end
-    redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
     book_comment.destroy
-    redirect_to request.referer
   end
 
   private
